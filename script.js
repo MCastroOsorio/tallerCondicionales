da2467292d5289d2815f07358e1886b1da003466
Mateo Castro <teocastro1405@gmail.com>
2024-08-20 09:52:18 -0500

    se crea la estructura del proyecto y se realizan 2 ejercicios

---
+++ script.js
@@ -0,0 +1,16 @@
+//Ejercicio 1
+
+let edad = prompt("Ingrese su edad: ");
+if(edad >= 18) {
+    console.log('Eres mayor de edad')
+} else {
+    console.log('Eres menor de edad')
+}
+
+//Ejercicio 2
+let numeroUno = prompt('Ingrese un numero')
+if (numeroUno >= 0){
+    console.log('El numero es positivo')
+} else{
+    console.log ('El numero es negativo')
+}
